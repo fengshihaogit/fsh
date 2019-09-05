954eda054e918a7956970598ec40db3bfcff0017
fsh <15731098022@163.com>
2019-09-05 08:19:57 +0800

    new one

---
+++ Tests.java
@@ -0,0 +1,15 @@
+package com.fsh.test;
+
+import org.junit.Test;
+
+/**
+ * @作者：Mr.冯
+ * @日期：2019年9月5日
+ */
+public class Tests {
+
+	@Test
+	public void tesets(){
+		System.out.println("helloword");
+	}
+}
